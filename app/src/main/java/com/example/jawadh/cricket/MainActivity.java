24dfce6140443a26323d7ec6553b691fe695a74e
Jawadh-Salih <jawadhsalih.4th@gmail.com>
2015-09-04 17:22:47 +0530

    Dynamic Players' List is added

--- app/src/main/java/com/example/jawadh/cricket/MainActivity.java
+++ app/src/main/java/com/example/jawadh/cricket/MainActivity.java
@@ -132,7 +132,7 @@ public class MainActivity extends ActionBarActivity {
                         return message;
                     }
 
-                    return "System Error";
+                    return "System Error"; // this will give a not null object
                 }
                 @Override
                 protected void onPostExecute(Object o) {
